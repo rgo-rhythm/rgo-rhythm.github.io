bfaeb6424635171ff000a88973ba91d9a7025a6d
studio-luke <goalone4r@postech.ac.kr>
2019-11-19 23:51:34 +0900

    made the pop-up modal

--- gtlove/date.js
+++ gtlove/date.js
@@ -1,37 +1,54 @@
-        function date(offset=0){
-            var today = new Date();
-            var month = today.getMonth() + 1
-            var date = today.getDate() + offset
-            
-            return month.toString() + "/" + date.toString();
+function date(offset){
+    var today = new Date();
+    var month = today.getMonth() + 1;
+    var date = today.getDate() + offset;
+    
+    return month.toString() + "/" + date.toString();
+}
+
+function printTimetable(){
+    var i, j;
+    var t = document.querySelector("#time_table");
+    var NUM_DATE = 3;
+    var contents = "";
+
+
+    contents += '<tr><td>시간</td>';
+    for(j = 0; j < NUM_DATE; j++){
+        contents += '<td>';
+        contents += date(j);
+        contents += '</td>';
+    }
+    contents += "</tr>"
+
+
+    for (i = 0; i < 24; i++){
+        contents += '<tr>\n';
+         contents += `<td>${i}:00~${i+1}:00</td>`;
+        for(j = 0; j < NUM_DATE; j++){
+            contents += '<td onclick="reserve(this);">';
+            // contents += 'a';
+            contents += '</td>';
         }
+        contents += '<tr>\n';
+    }
 
-        function printTimetable(){
-            var i, j;
-            var t = document.querySelector("#time_table");
-            const NUM_DATE = 3;
-            var contents = "";
+    t.innerHTML = contents;
+}
 
+function reserve(cell){
+    var modal = document.getElementById("rsv_spec");
+    var span = document.getElementsByClassName("close")[0];
 
-            contents += `<tr><td>시간</td>`
-            for(j = 0; j < NUM_DATE; j++){
-                contents += '<td>';
-                contents += date(j);
-                contents += '</td>';
-            }
-            contents += "</tr>"
+    // cell.setAttribute
+    modal.style.display = "block";
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
 
-
-            for (i = 0; i < 24; i++){
-                contents += '<tr>\n';
-                 contents += `<td>${i}:00~${i+1}:00</td>`;
-                for(j = 0; j < NUM_DATE; j++){
-                    contents += '<td>';
-                    // contents += 'a';
-                    contents += '</td>';
-                }
-                contents += '<tr>\n';
-            }
-
-            t.innerHTML = contents;
-        }
+    document.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
